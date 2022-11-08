5e3a73eddd95e0d5f73ee4381a9177cf18d34528
WinuX <37900754+xWinuX@users.noreply.github.com>
2022-11-08 23:53:30 +0100

    P1 done

---
+++ C++Praktika/OpenGL P1/main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#define INITIAL_WINDOW_WIDTH  800
+#define INITIAL_WINDOW_HEIGHT 600
+
+//------------------------------
+// glfw: whenever the window size changed (by OS or user resize input) this callback function executes
+//------------------------------
+void framebufferSizeCallback(GLFWwindow* window, const int width, const int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+//------------------------------
+// Process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+//------------------------------
+void processInput(GLFWwindow* window)
+{
+    // Close Window if escape key is pressed
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) { glfwSetWindowShouldClose(window, true); }
+}
+
+float getCurrentTime() { return static_cast<float>(glfwGetTime()); }
+
+float sin01(const float x) { return (sin(x) + 1.0f) / 2.0f; }
+
+int main()
+{
+    //-------------------------------
+    // Initialize and configure
+    // ------------------------------
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // ------------------------------
+    // GLFW Window creation
+    // ------------------------------
+    GLFWwindow* window = glfwCreateWindow(INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT, "SAE OpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+    // ------------------------------
+    // GLAD: load all OpenGL function pointers
+    // ------------------------------
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // ------------------------------
+    // Render Loop
+    // ------------------------------
+    float currentTime = getCurrentTime();
+    while (!glfwWindowShouldClose(window))
+    {
+        //------------------------------
+        // Time
+        //------------------------------
+        const float newTime   = getCurrentTime();
+        float       deltaTime = getCurrentTime() - currentTime;
+        currentTime           = newTime;
+
+        //------------------------------
+        // Input
+        //------------------------------
+        processInput(window);
+
+        //------------------------------
+        // Render
+        //------------------------------
+        glClearColor(sin01(currentTime), 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //------------------------------
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        //------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    //------------------------------
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    //------------------------------
+    glfwTerminate();
+    return 0;
+}
