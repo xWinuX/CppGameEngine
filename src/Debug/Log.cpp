bb5d5a9864397e3fe0e3a14ee9bbc696022d05fc
WinuX <edwin.baumann23@gmail.com>
2022-12-19 15:20:59 +0100

    fixed git ignore

---
+++ src/Debug/Log.cpp
@@ -0,0 +1,15 @@
+﻿#include "Log.h"
+
+#include <iostream>
+#include <ostream>
+
+void Debug::Log::Message(const std::string& message)
+{
+    std::cout << message << std::endl;
+}
+
+void Debug::Log::Error(const std::string& message)
+{
+    std::cerr << message << std::endl;
+}
+
---
+++ src/Debug/Log.h
@@ -0,0 +1,11 @@
+﻿#include <string>
+
+namespace Debug
+{
+    class Log
+    {
+        public:
+            static void Message(const std::string& message);
+            static void Error(const std::string& message);
+    };
+}
