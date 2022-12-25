e51079d0eda936b9df1c354c57cc56ae69aa5d72
WinuX <37900754+xWinuX@users.noreply.github.com>
2022-12-25 18:29:41 +0100

    cleanup

--- src/Rendering/Uniform.h
+++ src/Rendering/Uniform.h
@@ -12,98 +12,41 @@ class Uniform
         const GLint   _location = -1;
         const T       _defaultValue;
         T             _value;
-        int           _setCount;
     public:
         Uniform():
             _defaultValue(),
-            _value()
-        {
-            Debug::Log::Message("asdasd");
-        }
+            _value() { }
 
         Uniform(const GLuint programID, const GLchar* uniformName, const GLint location, T defaultValue):
             _name(uniformName),
             _location(location),
             _defaultValue(defaultValue),
-            _value(_defaultValue)
-        {
-            Debug::Log::Message("asdasd");
+            _value(_defaultValue) { }
 
-        }
-
-        Uniform(const Uniform& other)
-            :
-            _name(other._name),
-            _location(other._location),
-            _defaultValue(other._defaultValue),
-            _value(other._value),
-            _setCount(other._setCount)
-        {
-            Debug::Log::Message("Copy");
-        }
-
-        Uniform(Uniform&& other) noexcept
-            :
-            _name(other._name),
-            _location(other._location),
-            _defaultValue(std::move(other._defaultValue)),
-            _value(std::move(other._value)),
-            _setCount(other._setCount)
-        {
-            Debug::Log::Message("Copy");
-        }
-
-        Uniform& operator=(const Uniform& other)
-        {
-            if (this == &other) return *this;
-            _name         = other._name;
-            _location     = other._location;
-            _defaultValue = other._defaultValue;
-            _value        = other._value;
-            _setCount     = other._setCount;
-            return *this;
-        }
-
-        Uniform& operator=(Uniform&& other) noexcept
-        {
-            if (this == &other) return *this;
-            _name         = other._name;
-            _location     = other._location;
-            _defaultValue = std::move(other._defaultValue);
-            _value        = std::move(other._value);
-            _setCount     = other._setCount;
-            return *this;
-        }
 
         // ReSharper disable once CppMemberFunctionMayBeStatic (No it's not...)
-        void Apply()  {  }
+        void Apply() { Debug::Log::Message("This should never appear"); }
 
-        void Set(T value)
-        {
-            _value = value;
-            _setCount += 1;
-        }
+        void Set(T value) { _value = value; }
 
         void Reset() { _value = _defaultValue; }
 };
 
 
-template<>
-inline void Uniform<glm::vec4>::Apply() 
+template <>
+inline void Uniform<glm::vec4>::Apply()
 {
     glUniform4f(_location, _value.x, _value.y, _value.z, _value.w);
 }
 
-template<>
-inline void Uniform<glm::mat4>::Apply() 
+template <>
+inline void Uniform<glm::mat4>::Apply()
 {
-    Debug::Log::Message(std::to_string(_setCount));
-    _setCount += 1;
     glUniformMatrix4fv(_location, 1, GL_FALSE, &_value[0][0]);
 }
 
-template<>
-inline void Uniform<int>::Apply() 
+template <>
+inline void Uniform<int>::Apply()
 {
     glUniform1i(_location, _value);
 }
